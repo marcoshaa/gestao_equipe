6bac3f11178f09062d7297588a28d78a874f8894
marcoshaa <m.andrade.assis@gmail.com>
2023-01-23 13:31:40 -0300

    cores/padrao/css

--- resources/views/site/questionario.blade.php
+++ resources/views/site/questionario.blade.php
@@ -9,13 +9,24 @@
             width:100%;
             padding:0px 10px;
         }
+        .total_largura{
+            width:100%;
+        }
+        .form_total{
+            display:flex;
+        }
     </style>
 @endsection
 
 @section('content')
     <div class="painel_forme_inicio">
         <div class="topo_info_questionario">
-            
+
+        </div>
+        <div class="form_total">
+            <form class="form_total total_largura">
+                <div class="total_largura"><span></span></div>
+            </form>
         </div>
     </div>
 @endsection
--- resources/views/site/template.blade.php
+++ resources/views/site/template.blade.php
@@ -6,6 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PLATAFORMA EDUCACIONAL LP</title>
     <style>
+        :root {
+            --background_site: #1e90ff;
+            --background_topo: #ffffff;
+            --cor_bt:#000;
+            --cor_bt_hover:#fff;
+            --text_padrao:#000;
+            --text_offPadrao:#010;
+            --cor_linha: linear-gradient(to right, rgb(235 129 23) 0%,rgb(6 251 0) 23%,rgb(0 154 0) 35%,rgb(0 80 0) 57%,rgba(0,2,2,1) 100%);
+        }
         .content{
             display: flex;
         }
@@ -14,6 +23,10 @@
             height: 45px;
         }
     </style>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="jquery-3.6.1.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://www.gstatic.com/charts/loader.js"></script>       
     @yield('style')
 </head>
 <body>
