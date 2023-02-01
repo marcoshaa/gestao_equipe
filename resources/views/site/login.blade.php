7cfdc3f880353c50028b8ff248865cf991626f60
Marcos <m.andrade.assis@gmail.com>
2023-02-01 00:57:10 -0300

    bd/tela

---
+++ resources/views/site/login.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <style>
+        :root{
+            --background_fundo: #145ca4b3;
+        }
+        html {
+            margin:0px;
+            padding: 0px;
+            background:var(--background_fundo);
+        }
+    </style>
+</head>
+<body>
+    <div>
+
+    </div>
+</body>
+</html>
